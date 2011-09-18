d949e625bf28baebcb52ccd0fa756da105764c79
Matt Sweeney <mattswe@gmail.com>
2011-09-18 01:58:43 -0700

    Naming, and building segments with the requirement that they be orthogonal (for now)

--- fec-spacing.py
+++ fec-spacing.py
@@ -2,11 +2,11 @@
 
 from itertools import izip
 
-class wall_segment(object):
+class wall_segments(object):
 	def __init__(self):
 		self.points = []
 	
-	def load_segment(self, corners):
+	def load_segments(self, corners):
 		segments = []
 		segments.append((corners[-1], corners[0]))
 		for segx, segy in izip(corners[::2], corners[1::2]):
@@ -17,11 +17,15 @@ class wall_segment(object):
 	def build_segment(self, segment):
 		print segment[0]
 		print segment[1]
-		print range(segment[0][1],segment[1][1])
-		print range(segment[0][0],segment[1][0])
+		if segment[0][1] == segment[1][1]:
+			length = abs(segment[0][0] - segment[1][0])
+			print length
+		else:
+			length = abs(segment[0][1] - segment[1][1])
+			print length			
 		print '\n'
 
 if __name__ == "__main__":
 	corners = [(0, 0), (100, 0), (100, 80), (40, 80), (40, 120), (0, 120)]
-	ws = wall_segment()
-	ws.load_segment(corners)
+	ws = wall_segments()
+	ws.load_segments(corners)
