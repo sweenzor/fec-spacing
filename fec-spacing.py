e763845f2fd804ffe4a25366cbb185e845e552f7
Matt Sweeney <mattswe@gmail.com>
2011-09-17 23:20:47 -0700

    wall_segment class

--- fec-spacing.py
+++ fec-spacing.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python
 
+class wall_segment(object):
+	def __init__(self):
+		self.points = []
+
+if __name__ = "__main__":
+	corners = [(0, 0), (100, 0), (100, 80), (40, 80), (40, 120), (0, 120)]
+	
